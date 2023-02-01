833f9d7944e41a6888f4563ef4d5de590eff39dd
Christella De Sousa <desousa.christella@proton.me>
2023-02-01 14:21:27 +0100

    Delete all code inside the gallery div in index Modify helmet in app.js to be able to show images Fix function to get works from the API Fix function to show works in the document

--- FrontEnd/assets/works.js
+++ FrontEnd/assets/works.js
@@ -1,40 +1,30 @@
-/*works = fetch("http://localhost:5678/api/works")
-.then(works => works.json());
-*/
-
-function getAllWorks(){
-    fetch ("http://localhost:5678/api/works")
-        .then(function(res) {
-            if (res.ok) {
-                console.log("bojn");
-                return res.json();
-            }
-        })
-        .then(function(value) {
-            addWork(value);
-            console.log(value);
-        })
-        .catch(function(err) {
-        // Une erreur est survenue
-        });
+async function getAllWorks() {
+try {
+    const res = await fetch("http://localhost:5678/api/works");
+    if (res.ok) {
+      const works = await res.json();
+      addWorksToDocument(works);
+    }
+  } catch (err) {
+    console.error(err);
+  };
 }
 
-function addWork(works) {
-    works.forEach(function(){
-    const gallery = document.getElementsByClassName("gallery");
-    console.log("g");
-    //création balises
-    const figure = document.createElement("figure");
-    const img = document.createElement("img");
-    const caption = document.createElement("figcaption");
-    //lie balises
-    img.src = works.imageUrl;
-    img.setAttribute("alt", works.title);
-    caption.innerText = works.title;
-    //ajout à page web
-    //gallery.appendChild(figure);
-    console.log("gallery doesn't work");
-    figure.appendChild(img, caption);
+function addWorksToDocument(works) {
+    works.forEach(function(work) {
+        const gallery = document.getElementById("gallery");
+        const figure = document.createElement("figure");
+        const img = document.createElement("img");
+        const caption = document.createElement("figcaption");
+
+        img.src = work.imageUrl;
+        caption.innerText = work.title;
+
+        img.setAttribute("alt", work.title);
+
+        figure.appendChild(img);
+        figure.appendChild(caption);
+        gallery.appendChild(figure);
     });
 }
 
