512d95263a6ef652377c9c239bb56c1c5f9688fa
Christella De Sousa <desousa.christella@proton.me>
2023-02-01 15:06:47 +0100

    Modify indentation

--- FrontEnd/assets/js/works.js
+++ FrontEnd/assets/js/works.js
@@ -1,31 +1,31 @@
 async function getAllWorks() {
-try {
-    const res = await fetch("http://localhost:5678/api/works");
-    if (res.ok) {
-      const works = await res.json();
-      addWorksToDocument(works);
-    }
-  } catch (err) {
-    console.error(err);
-  };
+  try {
+      const res = await fetch("http://localhost:5678/api/works");
+      if (res.ok) {
+        const works = await res.json();
+        addWorksToDocument(works);
+      }
+    } catch (err) {
+      console.error(err);
+    };
 }
 
 function addWorksToDocument(works) {
-    works.forEach(function(work) {
-        const gallery = document.getElementById("gallery");
-        const figure = document.createElement("figure");
-        const img = document.createElement("img");
-        const caption = document.createElement("figcaption");
+  works.forEach(function(work) {
+      const gallery = document.getElementById("gallery");
+      const figure = document.createElement("figure");
+      const img = document.createElement("img");
+      const caption = document.createElement("figcaption");
 
-        img.src = work.imageUrl;
-        caption.innerText = work.title;
+      img.src = work.imageUrl;
+      caption.innerText = work.title;
 
-        img.setAttribute("alt", work.title);
+      img.setAttribute("alt", work.title);
 
-        figure.appendChild(img);
-        figure.appendChild(caption);
-        gallery.appendChild(figure);
-    });
+      figure.appendChild(img);
+      figure.appendChild(caption);
+      gallery.appendChild(figure);
+  });
 }
 
 getAllWorks();
