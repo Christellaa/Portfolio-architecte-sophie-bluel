c19c567ebc564a806ba0bf35aa7656a83a914b34
Christella De Sousa <desousa.christella@proton.me>
2023-02-08 18:42:03 +0100

    Delete console.log

--- FrontEnd/assets/js/currentPage.js
+++ FrontEnd/assets/js/currentPage.js
@@ -1,5 +1,4 @@
 const links = document.querySelectorAll(".navLinks a").forEach(link => {
-  console.log(link.href, window.location.href);
     if (link.href === window.location.href) {
     link.classList.add("currentLink");
   };
