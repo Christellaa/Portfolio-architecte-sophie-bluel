9e7431f622bbe084ca7f20196716d4029836635d
Christella De Sousa <desousa.christella@proton.me>
2023-02-16 15:00:54 +0100

    Add function to go to the second part of the modal Add function to go back to the first part of the modal

--- FrontEnd/assets/js/modal.js
+++ FrontEnd/assets/js/modal.js
@@ -5,18 +5,49 @@ const stopPropagation = function (e) {
     e.stopPropagation();
 }
 
+//open modalGallery
 const openModal = function (e) {
     e.preventDefault();
-    currentModal = document.querySelector(e.target.getAttribute("href"));
+    currentModal = document.querySelector("#modal");
     currentModal.style.display = "flex";
     currentModal.classList.add("flexCenter");
+    document.querySelector(".modalGallery").style.display = "block";
+    document.querySelector(".modalAddWork").style.display = "none";
     currentModal.removeAttribute("aria-hidden");
     currentModal.setAttribute("aria-modal", "true");
     currentModal.addEventListener("click", closeModal);
     currentModal.querySelector(".fa-xmark").addEventListener("click", closeModal);
-    currentModal.querySelector(".modal").addEventListener("click", stopPropagation);
+    currentModal.querySelector(".modalGallery").addEventListener("click", stopPropagation);
 }
 
+const openModalButton = document.querySelector("#openModalButton");
+openModalButton.addEventListener("click", openModal);
+
+//open modalAddWork
+const addPicture = function (e) {
+    e.preventDefault();
+    let modalGallery = document.querySelector(".modalGallery");
+    let modalAddWork = document.querySelector(".modalAddWork");
+    modalGallery.style.display = "none";
+    modalAddWork.style.display = "block";
+    modalAddWork.querySelector(".fa-xmark").addEventListener("click", closeModal);
+    modalAddWork.addEventListener("click", stopPropagation);
+}
+
+const addWorkButton = document.querySelector("#addWorkButton");
+addWorkButton.addEventListener("click", addPicture);
+
+//go back to modalGallery
+const goBackToModalGallery = function (e) {
+    e.preventDefault();
+    document.querySelector(".modalGallery").style.display = "block";
+    document.querySelector(".modalAddWork").style.display = "none";
+}
+
+const backToModalGallery = document.querySelector(".fa-arrow-left");
+backToModalGallery.addEventListener("click", goBackToModalGallery);
+
+//close modal
 const closeModal = function (e) {
     e.preventDefault();
     currentModal.style.display = "none";
@@ -24,13 +55,10 @@ const closeModal = function (e) {
     currentModal.removeAttribute("aria-modal");
     currentModal.removeEventListener("click", closeModal);
     currentModal.querySelector(".fa-xmark").removeEventListener("click", closeModal);
-    currentModal.querySelector(".modal").removeEventListener("click", stopPropagation);
+    currentModal.querySelector(".modalGallery").removeEventListener("click", stopPropagation);
+    currentModal.querySelector(".modalAddWork").removeEventListener("click", stopPropagation);
     currentModal = null;
 }
-
-document.querySelectorAll(".modifyLink").forEach(a => {
-    a.addEventListener("click", openModal);
-})
 
 //close modal by escape button
 window.addEventListener("keydown", function (e) {
