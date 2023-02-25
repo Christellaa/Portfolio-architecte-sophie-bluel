745b542d0aa3e38a13734e0c832ae8ea05b1f3ac
Christella De Sousa <desousa.christella@proton.me>
2023-02-25 12:57:32 +0100

    Delete token from sessionStorage when clicking on the link

--- FrontEnd/assets/js/logout.js
+++ FrontEnd/assets/js/logout.js
@@ -4,3 +4,5 @@ let logOut = document.getElementById("login-logout");
 
 if(tokenToDelete !== "" && tokenToDelete !== null) {
     logOut.innerHTML = "Logout";
+    logOut.addEventListener("click", (e) => {
+        sessionStorage.removeItem("token");
