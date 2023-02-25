76e20e663ccdc4ed9cbab0a269fedadf30b41405
Christella De Sousa <desousa.christella@proton.me>
2023-02-25 12:57:04 +0100

    Change the text of login link when logged in

--- FrontEnd/assets/js/logout.js
+++ FrontEnd/assets/js/logout.js
@@ -1,3 +1,6 @@
 const tokenToDelete = sessionStorage.getItem("token");
 
 let logOut = document.getElementById("login-logout");
+
+if(tokenToDelete !== "" && tokenToDelete !== null) {
+    logOut.innerHTML = "Logout";
